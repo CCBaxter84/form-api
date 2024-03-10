e7779d77d4c86d669cb74cdc3d75eed36bbb50e4
CCBaxter84 <troyceclark@gmail.com>
2024-03-09 17:19:01 -0700

    Added enums for typing

---
+++ enums.js
@@ -0,0 +1,16 @@
+export const Queries = Object.freeze({
+  limit: "limit",
+  afterDate: "afterDate",
+  beforeDate: "beforeDate",
+  offset: "offset",
+  status: "status",
+  includeEditLink: "includeEditLink",
+  sort: "sort"
+})
+
+export const Conditions = Object.freeze({
+  equals: "equals",
+  does_not_equal: "does_not_equal",
+  greater_than: "greater_than",
+  less_than: "less_than"
+})
