e3a3b2d5a08b5222bbf286f852a5f263a4aea681
CCBaxter84 <troyceclark@gmail.com>
2024-03-09 17:26:37 -0700

    Added endpoint

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 import express from "express"
 import "dotenv/config"
+import fetch from "node-fetch"
+import { getFilteredForms, getQueryParamsAsString } from "./helpers.js"
 
 const app = express()
+app.use(express.json())
+
+app.get("/:formId/filteredResponses", async (req, res) => {
+  try {
+    const { formId } = req.params
+    const stringifiedQueries = getQueryParamsAsString(req.query)
+    const response = await fetch(`${process.env.URL}/${formId}/submissions${stringifiedQueries}`, {
+      headers: {
+        "Authorization": `Bearer ${process.env.API_KEY}`,
+        "Content-Type": "application/json"
+      }
+    })
+    const data = await response.json()
+    const filteredData = getFilteredForms(data, req.query.filters)
+    res.status(200).send(filteredData)
+  } catch(e) {
+    console.log(e)
+    res.status(404).send("Not found")
+  }
+})
+
 app.listen(process.env.PORT)
 console.log(`Server is listening on port ${process.env.PORT}`)
